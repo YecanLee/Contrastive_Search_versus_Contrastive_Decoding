edfbb028f983d9dbd6469208605e49a46dfc5d18
meimingweili <yecanlee@yahoo.com>
2024-06-04 19:32:34 +0000

    [Add]research modified .py and results

--- scripts/measure_coherence.sh
+++ scripts/measure_coherence.sh
@@ -1,3 +1,35 @@
 CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
     --opt_model_name facebook/opt-2.7b\
-    --test_path ../inference_results/gpt2-xl/wikinews/wikinews_contrastive_k_5_alpha_0.6_result.json
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_5_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_10_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_15_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_5_alpha_0.7_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_10_alpha_0.7_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_15_alpha_0.7_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_5_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_10_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
+    --opt_model_name facebook/opt-2.7b\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_15_alpha_0.6_result.json
--- scripts/measure_mauve.sh
+++ scripts/measure_mauve.sh
@@ -1,2 +1,26 @@
 CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
-    --test_path ../inference_results/gpt2-xl/wikinews/wikinews_contrastive_k_5_alpha_0.6_result.json
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_5_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_10_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/book/book_adaptive_contrastive_k_15_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_5_alpha_0.7_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_10_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_15_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_5_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_10_alpha_0.6_result.json
+
+CUDA_VISIBLE_DEVICES=0 python ../measure_diversity_mauve_gen_length.py\
+    --test_path ../generated_resutls/wikitext/wikitext_adaptive_contrastive_k_15_alpha_0.6_result.json
