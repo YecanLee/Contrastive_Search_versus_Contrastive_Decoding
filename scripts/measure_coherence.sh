9a008fafc116bb98e53664478b2f253cd7c4de42
meimingweili <yecanlee@yahoo.com>
2024-06-07 15:58:11 +0000

    [Update]coherence sh

--- scripts/measure_coherence.sh
+++ scripts/measure_coherence.sh
@@ -16,11 +16,11 @@ CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
 
 CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
     --opt_model_name facebook/opt-2.7b\
-    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_10_alpha_0.7_result.json
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_10_alpha_0.6_result.json
 
 CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
     --opt_model_name facebook/opt-2.7b\
-    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_15_alpha_0.7_result.json
+    --test_path ../generated_resutls/wikinews/wikinews_adaptive_contrastive_k_15_alpha_0.6_result.json
 
 CUDA_VISIBLE_DEVICES=0 python ../compute_coherence.py\
     --opt_model_name facebook/opt-2.7b\
